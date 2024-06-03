438898e2fa2008395ba2012022a06d0252a5aaf4
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-03 13:57:56 -0600

    strings lab phase 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const  excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + '!'
+
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser[6]
+
+const shortGreeting = 'Welcome, ' + firstInitial + restOfName + '!'
